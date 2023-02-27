8cdd4e56a83ac7b63e6a4af364cc93d69d594e00
ilike3p <39431924+ilike3p@users.noreply.github.com>
2023-02-26 16:09:40 -0800

    fix

--- utils/connection.js
+++ utils/connection.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: "127.0.0.1",
+  host: "localhost",
   user: "root", // update MySQL user name - default user name is "root"
   port: 3306,
   password: "tray1963", // update MySQL password
